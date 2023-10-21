6b0a6b44bef791336b4e837243e6a9d3080bda0b
samarjit-singh <samarjitbravo@gmail.com>
2023-10-22 00:09:37 +0530

    created landing page

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Logo from "./logo";
+
+const Footer = () => {
+  return (
+    <div className="flex items-center w-full p-6 bg-background z-50">
+      <Logo />
+    </div>
+  );
+};
+
+export default Footer;
