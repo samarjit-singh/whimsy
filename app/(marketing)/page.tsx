22f62bc063bd3003b4eecbccf45c82e3c675d5d4
samarjit-singh <samarjitbravo@gmail.com>
2023-10-22 22:32:23 +0530

    created navbar in landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,7 +6,9 @@ const MarketingPage = () => {
   return (
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
-        <Heading />
+        {/* page tile */}
+        <Heading /> 
+        {/* images */}
         <Heroes />
       </div>
       <Footer />
