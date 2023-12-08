3c257fc37ae0456ca7301075816c765a5e00b77e
samarjit-singh <samarjitbravo@gmail.com>
2023-12-08 16:42:19 +0530

    add delete functionality in navbar

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -9,6 +9,7 @@ import { MenuIcon } from "lucide-react";
 
 import { Title } from "./title";
 import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 interface NavbarPorps {
   isCollapsed: boolean;
@@ -24,8 +25,11 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarPorps) => {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -46,6 +50,9 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarPorps) => {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
       {document.isArchived && <Banner documentId={document._id} />}
