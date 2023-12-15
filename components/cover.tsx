afeb28c5d32d737c1077e60d6aef7342ebc9807d
samarjit-singh <samarjitbravo@gmail.com>
2023-12-15 17:08:02 +0530

    add remove cover and update cover func

--- components/cover.tsx
+++ components/cover.tsx
@@ -1,8 +1,15 @@
 "use client";
 
 import Image from "next/image";
+import { ImageIcon, X } from "lucide-react";
+import { useMutation } from "convex/react";
+import { useParams } from "next/navigation";
 
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { useCoverImage } from "@/hooks/use-cover-image";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
 
 interface CoverImageProps {
   url?: string;
@@ -10,6 +17,16 @@ interface CoverImageProps {
 }
 
 export const Cover = ({ url, preview }: CoverImageProps) => {
+  const params = useParams();
+  const coverImage = useCoverImage();
+  const removeCoverImage = useMutation(api.documents.removeCoverImage);
+
+  const onRemove = () => {
+    removeCoverImage({
+      id: params.documentId as Id<"documents">,
+    });
+  };
+
   return (
     <div
       className={cn(
@@ -20,6 +37,28 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
     >
       {!!url && ( // double !! will convert the url string to boolean
         <Image src={url} fill alt="Cover" className="object-cover" />
+      )}
+      {url && !preview && (
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+          <Button
+            onClick={coverImage.onOpen}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Change Cover
+          </Button>
+          <Button
+            onClick={onRemove}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <X className="h-4 w-4 mr-2" />
+            Remove
+          </Button>
+        </div>
       )}
     </div>
   );
