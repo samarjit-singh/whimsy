77cce370f39f9ca7b781b077780a557fe6146167
samarjit-singh <samarjitbravo@gmail.com>
2023-12-15 16:50:43 +0530

    render cover image

---
+++ components/cover.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+
+interface CoverImageProps {
+  url?: string;
+  preview?: boolean;
+}
+
+export const Cover = ({ url, preview }: CoverImageProps) => {
+  return (
+    <div
+      className={cn(
+        "relative w-full h-[35vh] group",
+        !url && "h-[12vh]",
+        url && "bg-muted"
+      )}
+    >
+      {!!url && ( // double !! will convert the url string to boolean
+        <Image src={url} fill alt="Cover" className="object-cover" />
+      )}
+    </div>
+  );
+};
